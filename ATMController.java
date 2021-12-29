0402b4f646f79feec6e3b74fa250a4ea2b226d5c
junshin163 <78077147+junshin163@users.noreply.github.com>
2021-12-29 22:38:20 +0900

    Update ATMController.java

--- ATMController.java
+++ ATMController.java
@@ -3,11 +3,9 @@ import java.util.*;
 class ATMController {
   private HashMap<String, Card> compatibleCards;
 
-
   public ATMController(HashMap<String, Card> compatibleCards){
     this.compatibleCards = compatibleCards;
   }
-
 
   public HashMap<String, Card> insertCardPIN(String cardName, int attemptedPIN){  //returns String containing account names IF PIN is correct
     Card c = compatibleCards.get(cardName);
@@ -19,7 +17,6 @@ class ATMController {
       return null;
     }
   }
-  
   
   /* we return the set of strings containing card names instead of actual card objects
   to not give access to any cards unless the user has the correct PIN for a card: */
