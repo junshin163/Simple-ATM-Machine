38ceda2ec1bc5b69138d7e7dad959530f020f816
junshin163 <78077147+junshin163@users.noreply.github.com>
2021-12-29 22:36:53 +0900

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,26 @@
+class Account {
+  private String accountName;
+  private int balance;
+
+
+  public Account(String accountName, int balance){
+    this.accountName = accountName;
+    this.balance = balance;
+  }
+  
+  public int seeBalance(){
+    return balance;
+  }
+
+  public void deposit(int amount){
+    balance += amount;
+  }
+
+  public void withdraw(int amount){
+    balance -= amount;
+  }
+
+  public String getAccountName(){
+    return accountName;
+  }
+}
